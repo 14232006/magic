0628afa8411bfd4ce2c09a5a980db61bade300b0
14232006 <shristibirthday@gmail.com>
2024-08-14 15:18:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const magicSection = document.querySelector('.magic-section');
+const wand = document.getElementById("wand");
+const tiles = document.querySelectorAll(".tile");
+
+const xy = (x, y) => ({ x, y });
+const px = value => `${value}px`;
+const deg = value => `${value}deg`;
+const clamp = (value, min, max) => Math.max(Math.min(value, max), min);
+
+const updateMouse = (mouseX, mouseY) => {
+  const { innerWidth: windowWidth, innerHeight: windowHeight } = window;
+  
+  const mouse = {
+    position: xy(mouseX, mouseY),
+    decimal: xy(mouseX / windowWidth, mouseY / windowHeight),
+    multiplier: xy(1.3, 0.4),
+    offset: xy(windowWidth * -0.15, windowHeight * 0.1),
+    modifiedPosition: xy(0, 0)
+  }
+  
+  mouse.modifiedPosition.x = mouse.position.x * mouse.multiplier.x + mouse.offset.x;  
+  mouse.modifiedPosition.y = mouse.position.y * mouse.multiplier.y + mouse.offset.y;  
+  
+  return mouse;
+}
+
+const revealImages = mouseX => {
+  for(const tile of tiles) {
+    const dimensions = tile.getBoundingClientRect();
+    const relativeMouseX = mouseX - dimensions.left;
+    const mouseXAsDecimal = clamp(relativeMouseX / dimensions.width, 0, 1);
+    
+    const opacity = mouseXAsDecimal;
+    const blur = 1 - mouseXAsDecimal;
+    
+    tile.style.setProperty("--opacity", opacity);
+    tile.style.setProperty("--blur", blur);
+  }
+}
+
+const getWandStyles = mouse => ({
+  left: px(mouse.modifiedPosition.x),
+  top: px(mouse.modifiedPosition.y),
+  rotate: deg(mouse.decimal.x * 20 - 10)
+});
+
+magicSection.onmousemove = e => {
+  const mouse = updateMouse(e.clientX, e.clientY);  
+  const wandStyles = getWandStyles(mouse);
+  
+  wand.animate(wandStyles, { duration: 400, fill: "forwards" });
+  
+  revealImages(mouse.modifiedPosition.x);
+}
